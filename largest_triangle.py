2ac9f73b00cd61655ab21f13aa835f745d35df72
Rrs2802 <97283010+Rrs2802@users.noreply.github.com>
2023-08-05 11:39:31 +0530

    Add files via upload

---
+++ largest_triangle.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def solve(H) :
+    s,i,m = [],0,0
+    while i < len(H) :
+        if not s or H[i] > H[s[-1]] :
+            s.append(i)
+            i += 1
+        else :
+            t = s.pop()
+            a = H[t] * ((i - s[-1] -1)  if s else i)
+            if a > m :
+                m = a
+                
+    while s :
+        t = s.pop()
+        a = H[t] * ((i - s[-1] -1)  if s else i)
+        if a > m :
+            m = a
+        
+    return m           
+
+N = int(input())
+H = list(int(_) for _ in input().split())
+
+print(solve(H))
+
